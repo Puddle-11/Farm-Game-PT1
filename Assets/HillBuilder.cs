492907ef08c3d87179170bfbc27fb55373655c8c
Puddle <Puddleglumstudio@gmail.com>
2024-12-11 22:05:47 -0500

    Added luts and updated hill builder

--- Assets/HillBuilder.cs
+++ Assets/HillBuilder.cs
@@ -13,7 +13,12 @@ public class HillBuilder : MonoBehaviour
     [SerializeField] private int resolution;
     [SerializeField] private float noiseScale;
     [SerializeField] private float noiseWeight;
-
+    [SerializeField] private bool ramp;
+    [SerializeField] private int rampLength;
+    [SerializeField] private int rampStartIndex;
+    [SerializeField] private float rampWidth;
+    [SerializeField] private AnimationCurve rampCurve;
+    [SerializeField] private AnimationCurve widthCurve;
 
     [SerializeField] private Color stone;
     [SerializeField] private Color grass;
@@ -62,6 +67,7 @@ public class HillBuilder : MonoBehaviour
             float y = radius * Mathf.Sin(2 * Mathf.PI * i / resolution);
 
             fPoints[i] = new Vector3(x, 0, y);
+
             tPoints[i] = new Vector3(x, height, y);
 
             float offset = SampleNoise(new Vector2(fPoints[i].x, fPoints[i].z));
@@ -73,7 +79,7 @@ public class HillBuilder : MonoBehaviour
         }
 
 
-        GenerateTrianglePoints(out verts, out colors);
+        CalculateVerticies(out verts, out colors);
         tris = GenerateTriangles();
         UpdateMesh();
     }
@@ -102,7 +108,7 @@ public class HillBuilder : MonoBehaviour
 
 
     }
-    private void GenerateTrianglePoints(out Vector3[] _verts, out Color[] _col)
+    private void CalculateVerticies(out Vector3[] _verts, out Color[] _col)
     {
 
         List<Vector3> res = new List<Vector3>();
@@ -138,9 +144,121 @@ public class HillBuilder : MonoBehaviour
             //==========================================================
 
         }
+        if (ramp)
+        {
+            int startIndex = rampStartIndex % resolution;
+            int currIndex;
+            int nextIndex;
+
+            rampLength = Mathf.Clamp(rampLength, 0, resolution);
+            Vector3 p1 = Vector3.zero;
+            Vector3 p2 = Vector3.zero;
+            Vector3 p3 = Vector3.zero;
+            Vector3 p4 = Vector3.zero;
+
+            Vector3 bp1 = Vector3.zero;
+            Vector3 bp2 = Vector3.zero;
+
+            for (int i = 0; i < rampLength; i++)
+            {
+                currIndex = (startIndex + i) % resolution;
+                nextIndex = (currIndex + 1) % resolution;
+                float p1Height = Mathf.Clamp(rampCurve.Evaluate(i / (float)rampLength), 0, 1) * height;
+                float p2Height = Mathf.Clamp(rampCurve.Evaluate((i + 1) / (float)rampLength), 0, 1) * height;
+
+                Vector3 correctedP1 = Vector3.Lerp(fPoints[currIndex], new Vector3(tPoints[currIndex].x, fPoints[currIndex].y, tPoints[currIndex].z), p1Height/height);
+                Vector3 correctedP2 = Vector3.Lerp(fPoints[nextIndex], new Vector3(tPoints[nextIndex].x, fPoints[nextIndex].y, tPoints[nextIndex].z), p2Height / height);
+
+         
+
+
+                //on wall
+                p1 = correctedP1 + new Vector3(0, p1Height, 0);
+                p2 = correctedP2 + new Vector3(0, p2Height, 0);
+
+
+
+                //off wall
+                if (i == 0)
+                {
+                    p3 = p1 + fPoints[currIndex].normalized * widthCurve.Evaluate((i + 1) / (float)rampLength) * rampWidth;
+                    bp1 = fPoints[currIndex] + fPoints[currIndex].normalized * widthCurve.Evaluate((i + 1) / (float)rampLength) * rampWidth;
+                }
+               
+                p4 = p2 + fPoints[nextIndex].normalized * widthCurve.Evaluate((i + 1) / (float)rampLength) * rampWidth;
+                bp2 = fPoints[nextIndex] + fPoints[nextIndex].normalized * widthCurve.Evaluate((i + 1) / (float)rampLength) * rampWidth;
+
+
+
+
+                if (i == 0)
+                {
+                    //draw start cap
+                    res.Add(p1);
+                    res.Add(bp1);
+                    res.Add(fPoints[currIndex]);
+
+                    res.Add(p1);
+                    res.Add(p3);
+                    res.Add(bp1);
+                    colorRes.AddRange(Enumerable.Repeat(stone, 6).ToArray());
+
+                }
+                 else if (i == rampLength - 1)
+                {
+                    //draw start cap
+                    res.Add(p2);
+                    res.Add(fPoints[nextIndex]);
+                    res.Add(bp2);
+
+                    res.Add(p4);
+                    res.Add(p2);
+                    res.Add(bp2);
+                    colorRes.AddRange(Enumerable.Repeat(stone, 6).ToArray());
+                }
+
+
+                //top surface
+                res.Add(p1);
+                res.Add(p2);
+                res.Add(p3);
+
+                res.Add(p2);
+                res.Add(p4);
+                res.Add(p3);
+                //==================
+                colorRes.AddRange(Enumerable.Repeat(grass, 6).ToArray());
+
+
+                //recalculate bottom verts
+                //outer wall
+                res.Add(bp1);
+                res.Add(p3);
+                res.Add(bp2);
+
+
+                res.Add(p4);
+                res.Add(bp2);
+                res.Add(p3);
+
+                //==================
+                colorRes.AddRange(Enumerable.Repeat(stone, 6).ToArray());
+
+                p3 = p4;
+                bp1 = bp2;
+            }
+
+
+            //res.Add(fPoints[(startIndex + 1) % resolution] + new Vector3(0, rampCurve.Evaluate(i + 1 / (float)rampLength) * height, 0));
+            //res.Add(fPoints[(startIndex + 1) % resolution] + new Vector3(0, rampCurve.Evaluate(i + 1 / (float)rampLength) * height, 0) +fPoints[(startIndex + 1) % resolution].normalized * rampWidth);
+            //res.Add(fPoints[(startIndex)] + fPoints[(startIndex)].normalized * rampWidth);
+
+
+        }
+
         _col = colorRes.ToArray();
         _verts = res.ToArray();
-       
+
     }
     private int[] GenerateTriangles()
     {
@@ -165,23 +283,6 @@ public class HillBuilder : MonoBehaviour
 
     }
 
-    private void OnDrawGizmosSelected()
-    {
-
-        DrawPointGizmos();
-
-    }
-    private void DrawPointGizmos()
-    {
-        if (tPoints == null || tPoints.Length <= 0)
-        {
-            return;
-        }
-        for (int i = 0; i < tPoints.Length; i++)
-        {
-            Gizmos.DrawSphere(tPoints[i] + transform.position, 0.1f);
-        }
 
 
-    }
 }
